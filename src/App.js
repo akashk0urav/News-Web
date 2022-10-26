1fd92ded3befc38aa2a9ff6962fd8eedb6d8c8e9
avnesh141 <94967840+avnesh141@users.noreply.github.com>
2022-10-26 16:47:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 import Navbar from "./Components/Navbar";
 import LoadingBar from "react-top-loading-bar";
 import NewsComponent from "./Components/NewsComponent";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,Navigate} from "react-router-dom";
 // 46bed2362890412aa99dfe1b47094e2c
 // f6a66865f73f414ea0f5eb3308ef3ef8;
 // 291c7235d7334846a451f4942a439414
